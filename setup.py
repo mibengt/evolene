1bf082860e4ed6ad6bdfae8bbfd41004c903be37
Jens Tinglev <tinglev@kth.se>
2017-04-12 13:20:37 +0200

    Minor dist fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-import os
 from setuptools import setup, find_packages
 
 VERSION = '0.1'
-NAME = 'evolene'
+PROJECT_NAME = 'evolene'
 
-DIST_NAME = '{}-{}.tar.gz'.format(NAME, VERSION)
-os.environ['DIST_NAME'] = DIST_NAME
-
-setup(name='evolene',
+setup(name=PROJECT_NAME,
       version=VERSION,
       description='KTH CD/CI pipeline tool',
       url='https://gita.sys.kth.se/Infosys/evolene',
