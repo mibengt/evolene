0c150807f061eec723c3a8942244ce941403ca2f
Patric Jansson <paddy@kth.se>
2017-06-27 14:11:36 +0200

    Message change

--- modules/pipeline_steps/abstract_pipeline_step.py
+++ modules/pipeline_steps/abstract_pipeline_step.py
@@ -69,8 +69,8 @@ class AbstractPipelineStep:
             error_func(message)
 
     def report_error_to_slack(self, message):
-        message = ('Error in build of image "{}" on build step "{}": {}'
-                   .format(Environment.get_image_name(), self.get_step_name(), message))
+        message = ('*{}*: Error building image.\n{}'
+                   .format(Environment.get_image_name(), message))
         Slack.send_to_slack(message)
 
     def run_pipeline_step(self, data):
