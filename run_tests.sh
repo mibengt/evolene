1e7a3cac9c91f3fdc76c0bf019aaed6d17c4ec9a
Patric Jansson <paddy@kth.se>
2017-11-26 14:09:16 +0100

    simpler testing

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+RUN_WITH_VIRTUALENV=TRUE
+
+virtualenv -v .tmp >/dev/null 2>&1 || {
+  echo "No virtualenv installed."
+  rm -rf .tmp
+  exit 1
+}
+rm -rf .tmp
+
+echo "Virtualenv environment 'env' used for tests."
+virtualenv env
+env/bin/pip install -r requirements.txt
 env/bin/green "test" -vv --run-coverage
