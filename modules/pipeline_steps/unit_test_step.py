9af1a9689daf8feeea323446339f7d48f33aa4a7
Patric Jansson <paddy@kth.se>
2018-08-23 11:03:55 +0200

    Bubbel sclasr

--- modules/pipeline_steps/unit_test_step.py
+++ modules/pipeline_steps/unit_test_step.py
@@ -37,5 +37,5 @@ class UnitTestStep(AbstractPipelineStep):
         try:
             Docker.run_unit_test_compose(compose_test_file, data)
         except Exception as ex:
-            raise PipelineException('*{}* Unit tests failed: \n{}\n\n:jenkins: {}/console'
+            raise PipelineException('*{}* Unit tests failed: \n{}\n\n:jenkins: {}console'
                                     .format(data[Data.IMAGE_NAME], ex.message.replace('`', ' '), Environment.get_build_url()))
