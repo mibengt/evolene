7d3de73522954b156a14617226a6b24f34ec74fd
Patric Jansson <paddy@kth.se>
2018-10-05 13:50:06 +0200

    clean up

--- modules/util/image_version_util.py
+++ modules/util/image_version_util.py
@@ -16,19 +16,3 @@ class ImageVersionUtil(object):
         if not re.match(r'^[0-9]+\.[0-9]+$', image_version):
             return False
         return True
-    
-    @staticmethod
-    def is_valid(image_version):
-        if ImageVersionUtil.use_patch_from_docker_conf():
-            return ImageVersionUtil.is_major_minor_patch(image_version)
-        return ImageVersionUtil.is_major_minor_only(image_version)
-    
-    @staticmethod
-    def use_patch_from_docker_conf():
-        if Environment.get_build_number() is None:
-            return False
-
-        if Environment.get_build_number() == "docker.conf":
-            return True
-        
-        return False
