489e18a17ec121dcb911b24de8f7e0a5722ddd05
Patric Jansson <paddy@kth.se>
2018-11-01 09:14:38 +0100

    1.7 intial

--- modules/pipeline_steps/setup_step.py
+++ modules/pipeline_steps/setup_step.py
@@ -6,6 +6,8 @@ from modules.util.environment import Environment
 
 class SetupStep(AbstractPipelineStep):
 
+
+
     def get_required_env_variables(self):
         return [Environment.SLACK_CHANNELS, Environment.SLACK_WEB_HOOK]
 
@@ -16,21 +18,27 @@ class SetupStep(AbstractPipelineStep):
         self.print_header()
         return data
 
+    def color(self, line, color='\033[0m')
+        print "{}{}\033[0m\n".format(color, line)
+    
+    def print_black(self, line):
+        self.color(line, color='\033[95m')
+
+    def print_pink(self, line):
+        self.print_black(line, color='\033[95m')
+
     def print_header(self):
-        print (
-            "\033[95m"
-            "                                                    \n"
-            "  ______                   _                        \n"
-            " |  ____|                 | |                       \n"
-            " | |__    __   __   ___   | |   ___   _ __     ___  \n"
-            " |  __|   \ \ / /  / _ \  | |  / _ \ | '_ \   / _ \ \n"
-            " | |____   \ V /  | (_) | | | |  __/ | | | | |  __/ \n"
-            " |______|   \_/    \___/  |_|  \___| |_| |_|  \___| \n"
-            "                                                    \n"
-            "\033[0m"
-            "****************************************************\n"
-            " Help make Evolene better!                          \n"
-            " https://gita.sys.kth.se/Infosys/evolene            \n"
-            "****************************************************\n"
-            "                                                    \n"
-        )
+        self.print_black("                                                    ")
+        self.print_pink("  ______                   _                        ")
+        self.print_pink(" |  ____|                 | |                       ")
+        self.print_pink(" | |__    __   __   ___   | |   ___   _ __     ___  ")
+        self.print_pink(" |  __|   \ \ / /  / _ \  | |  / _ \ | '_ \   / _ \ ")
+        self.print_pink(" | |____   \ V /  | (_) | | | |  __/ | | | | |  __/ ")
+        self.print_pink(" |______|   \_/    \___/  |_|  \___| |_| |_|  \___| ")
+        self.print_black("                                                    ")
+        self.print_black("                                                    ")
+        self.print_black("****************************************************")
+        self.print_black(" Help make Evolene better!                          ")
+        self.print_black(" https://gita.sys.kth.se/Infosys/evolene            ")
+        self.print_black("****************************************************")
+        self.print_black("                                                    ")
