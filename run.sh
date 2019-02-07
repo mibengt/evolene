2cef6ffc40577a34695e097856fc42c0fb728a2a
tinglev <tinglev@kth.se>
2019-02-07 13:58:53 +0100

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 EVOLENE_DIRECTORY=$EVOLENE_DIRECTORY PIPENV_PIPFILE=$EVOLENE_DIRECTORY/Pipfile \
-    pipenv run python $EVOLENE_DIRECTORY/run.py docker run-pipeline
+    pipenv run python $EVOLENE_DIRECTORY/run.py
