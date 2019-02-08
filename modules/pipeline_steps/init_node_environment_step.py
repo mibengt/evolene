b18a3fb93a1ea94f2d2c94b49a304bb64521a8d4
Jens Tinglev <tinglev@kth.se>
2019-02-08 07:50:36 +0100

    npm fixes

--- modules/pipeline_steps/init_node_environment_step.py
+++ modules/pipeline_steps/init_node_environment_step.py
@@ -4,6 +4,7 @@ from modules.pipeline_steps.abstract_pipeline_step import AbstractPipelineStep
 from modules.util.environment import Environment
 from modules.util.process import Process
 from modules.util.data import Data
+from modules.util import nvm
 
 class InitNodeEnvironmentStep(AbstractPipelineStep):
 
@@ -19,15 +20,15 @@ class InitNodeEnvironmentStep(AbstractPipelineStep):
     def run_step(self, data):
         conf_version = data[Data.NPM_CONF_NODE_VERSION]
         self.log.debug('Configured node version is: "%s"', conf_version)
-        nvm_installed_version = self.get_nvm_installed_version(conf_version)
+        nvm_installed_version = self.get_nvm_installed_version(conf_version, data)
         if nvm_installed_version.strip() == 'N/A':
             self.log.debug('Configured node version not installed; installing')
-            self.install_version(conf_version)
+            self.install_version(conf_version, data)
         self.log.debug('Node version %s installed with nvm', conf_version)
         return data
 
-    def get_nvm_installed_version(self, version):
-        return Process.run_with_output(f'nvm version {version}')
+    def get_nvm_installed_version(self, version, data):
+        return nvm.nvm_exec(data, f'nvm version {version}')
 
-    def install_version(self, version):
-        Process.run_with_output(f'nvm install {version}')
+    def install_version(self, version, data):
+        nvm.nvm_exec(data, f'nvm install {version}')
