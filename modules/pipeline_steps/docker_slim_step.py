e520db8ac79fb0bcb8bc44d3a29873302878dab9
Jens Tinglev <tinglev@kth.se>
2019-12-11 17:28:27 +0100

    docker slim now runs on image id

--- modules/pipeline_steps/docker_slim_step.py
+++ modules/pipeline_steps/docker_slim_step.py
@@ -14,20 +14,14 @@ class DockerSlimStep(AbstractPipelineStep):
         return []
 
     def get_required_data_keys(self):
-        return [pipeline_data.IMAGE_NAME, pipeline_data.IMAGE_VERSION, pipeline_data.SEM_VER]
+        return [pipeline_data.LOCAL_IMAGE_ID]
 
     def run_step(self, data):
         if environment.get_experimental():
-            # Tag the original image names
-            tag_step = TagImageStep()
-            data = tag_step.run_step(data)
             self.run_docker_slim(data)
             data[pipeline_data.IMAGE_NAME] = f'{data[pipeline_data.IMAGE_NAME]}.slim'
         return data
 
     def run_docker_slim(self, data):
-        image = image_version_util.prepend_registry(
-            image_version_util.get_image(data)
-        )
-        self.log.debug('Running /var/lib/jenkins/docker-slim build %s', image)
-        process.run_with_output(f'/var/lib/jenkins/docker-slim build {image}')
+        image_id = data[pipeline_data.LOCAL_IMAGE_ID]
+        process.run_with_output(f'/var/lib/jenkins/docker-slim build {image_id}')
