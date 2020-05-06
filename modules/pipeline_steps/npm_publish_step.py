765449e8797e9ec2a1f0ce9ec5b26b7ab380869d
Patric Jansson <paddy@kth.se>
2020-05-06 11:26:12 +0200

    prefix se.kth.

--- modules/pipeline_steps/npm_publish_step.py
+++ modules/pipeline_steps/npm_publish_step.py
@@ -1,6 +1,7 @@
 __author__ = 'tinglev'
 
 import json
+import os
 from modules.pipeline_steps.abstract_pipeline_step import AbstractPipelineStep
 from modules.util import pipeline_data, slack, nvm, environment
 from modules.util import file_util
@@ -19,13 +20,13 @@ class NpmPublishStep(AbstractPipelineStep):
 
     def should_auto_update(self, data):
         try:
-            major_minor_version = data[pipeline_data.PACKAGE_JSON]["publishVersionBase"]
+            major_minor_version = data[pipeline_data.PACKAGE_JSON]["se.kth.version"]
         except:
             return False
 
         if major_minor_version is not None:
             if major_minor_version.count('.') != 1:
-                raise ValueError('publishVersionBase must be onlys major.minor')
+                raise ValueError('se.kth.version must be onlys major.minor')
             self.log.info('Will auto increase patch version package.json based on : %s', major_minor_version)
             return True
         self.log.info('Will not auto increase patch version in package.json')
@@ -37,11 +38,11 @@ class NpmPublishStep(AbstractPipelineStep):
         # Read the last didgit = "1.2.[3]"
         patch_version_index = version.rfind(".") + 1
         patch_version = version[patch_version_index:]
-        self.log.debug('Patch version is: %s', patch_version)
+        self.log.debug('Next free patch version is: %s', patch_version)
         return int(patch_version)
 
     def get_auto_update_version(self, data):
-        major_minor_version = data[pipeline_data.PACKAGE_JSON]["publishVersionBase"]
+        major_minor_version = data[pipeline_data.PACKAGE_JSON]["se.kth.version"]
         updated_patch_version = self.get_latest_patch_version(data) + 1
         version = f"{major_minor_version}.{updated_patch_version}"
         self.log.info('Auto update resulted in npm publish using %s as version', version)
@@ -50,6 +51,10 @@ class NpmPublishStep(AbstractPipelineStep):
 
     def write_auto_updated_package_json(self, data):
         data[pipeline_data.PACKAGE_JSON]["version"] = self.get_auto_update_version(data)
+        data[pipeline_data.PACKAGE_JSON]["se.kth.gitBranch"] = environment.get_git_branch()
+        data[pipeline_data.PACKAGE_JSON]["se.kth.gitCommit"] = environment.get_git_commit()
+        data[pipeline_data.PACKAGE_JSON]["se.kth.buildDate"] = environment.get_time()
+        
         file_util.overwite('/package.json', json.dumps(data[pipeline_data.PACKAGE_JSON]))
         data[pipeline_data.NPM_VERSION_CHANGED] = True
        
