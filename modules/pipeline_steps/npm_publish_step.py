1606ed32fb6c089286df733493b648c196de20f3
Patric Jansson <paddy@kth.se>
2020-06-24 15:25:18 +0200

    Removed Slack when no publish

--- modules/pipeline_steps/npm_publish_step.py
+++ modules/pipeline_steps/npm_publish_step.py
@@ -45,8 +45,8 @@ class NpmPublishStep(AbstractPipelineStep):
             self.publish(data)
             slack.on_npm_publish(data[pipeline_data.NPM_PACKAGE_NAME], data[pipeline_data.NPM_PACKAGE_VERSION], data)
         else:
-            self.log.debug('Version hasnt changed, skipping publish')
-            slack.on_npm_no_publish(data[pipeline_data.NPM_PACKAGE_NAME], data[pipeline_data.NPM_PACKAGE_VERSION])
+            self.log.debug('Skipping npm publish, no version change.')
+            #slack.on_npm_no_publish(data[pipeline_data.NPM_PACKAGE_NAME], data[pipeline_data.NPM_PACKAGE_VERSION])
                                     
         return data
 
