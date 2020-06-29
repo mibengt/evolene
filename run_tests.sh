8b72640d118680a6cf2af26ea31754f2ec80ae41
Patric Jansson <paddy@kth.se>
2020-06-29 14:14:45 +0200

    short and install --dev

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-PIPENV_VERBOSITY=-1 WORKSPACE=./test/data pipenv run green -vv --run-coverage --failfast "test"
-#PIPENV_VERBOSITY=-1 WORKSPACE=./test/data pipenv run green --failfast "test"
+
+if [[ -z "${SKIP_INSTALL}" ]]; then
+  echo "To skip install run: SKIP_INSTALL=True ./run_tests.sh"
+  pipenv install --dev 
+fi
+
+if [[ -z "${SHORT}" ]]; then
+  echo "To get minimal ouput run: SHORT=True SKIP_INSTALL=True ./run_tests.sh"
+  PIPENV_VERBOSITY=-1 WORKSPACE=./test/data pipenv run green -vv --run-coverage --failfast "test"
+else
+  PIPENV_VERBOSITY=-1 WORKSPACE=./test/data pipenv run green --failfast "test"
+fi
