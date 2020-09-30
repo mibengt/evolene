d24668060831a62d523020ab59e7ad7f5fcf2fd9
Patric Jansson <paddy@kth.se>
2020-09-30 12:51:10 +0200

    Enable both npm and docker in one build.

--- run.py
+++ run.py
@@ -10,24 +10,27 @@ from modules.util import environment, file_util, slack
 import modules.util.log as log
 
 def select_and_run_pipeline():
+    
     logger = logging.getLogger(__name__)
     docker_conf = file_util.get_absolue_path('/docker.conf')
     has_docker_conf = os.path.isfile(docker_conf)
     npm_conf = file_util.get_absolue_path('/npm.conf')
     has_npm_conf = os.path.isfile(npm_conf)
+
     if has_docker_conf:
         pipeline = DockerDeployPipeline()
         pipeline.run_pipeline()
-        return
+
     if has_npm_conf:
         pipeline = NpmPipeline()
         pipeline.run_pipeline()
-        return
-    workspace = environment.get_project_root()
-    message = f'No docker.conf or npm.conf found for project {workspace}'
-    slack.send_to_slack(message)
-    logger.error(message)
-    sys.exit(1)
+
+    if has_docker_conf is False and has_npm_conf is False:
+        workspace = environment.get_project_root()
+        message = f'No docker.conf or npm.conf found for project {workspace}'
+        slack.send_to_slack(message)
+        logger.error(message)
+        sys.exit(1)
 
 def main():
     log.init_logging()
