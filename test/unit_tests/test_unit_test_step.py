74a5fde73d69d3d28dd7a7470b904baee4a80ebd
Patric Jansson <paddy@kth.se>
2020-11-06 13:12:46 +0100

    Show only unit test failure

--- test/unit_tests/test_unit_test_step.py
+++ test/unit_tests/test_unit_test_step.py
@@ -31,27 +31,34 @@ class UnitTestStepTests(unittest.TestCase):
 
         step = UnitTestStep()
 
-        excepted = '''
-        
-          1 passing (480ms)
-          1 failing
-        
-          1) Template handling
-               Path '/_monitor' should contain 'No env value for ENV_TEST is set.' when env 'ENV_TEST' is not set.:
-             AssertionError: expected '\n  - ENV_TEST: No env value for ENV_TEST is set. - Should return the cluster name.\n  - API Call: kth-azure-app - Should always return this applications name.\n  ' to include 'No env FAILJRED value for ENV_TEST is set'
-              at Context.<anonymous> (/tests/unit-tests/test-app.js:22:23)
-              at processTicksAndRejections (internal/process/task_queues.js:93:5)
-        
-        
-        
-        npm ERR! Test failed.  See above for more details.
-        kth-azure-app_web_1_1b99cff96784 exited with code 1
-        Aborting on container exit...
-        '''
         clean_text = step.remove_docker_compose_output(ci_output)
 
         self.assertFalse('web_1_1b99cff96784 | ' in clean_text)
         self.assertTrue('1 failing' in clean_text)
         
 
+    def test_slack_message_shortend(self):
+        ci_output = '''
+        web_1_1b99cff96784 | 
+        web_1_1b99cff96784 |   1 passing (480ms)
+        web_1_1b99cff96784 |   1 failing
+        web_1_1b99cff96784 | 
+        web_1_1b99cff96784 |   1) Template handling
+        web_1_1b99cff96784 |        Path '/_monitor' should contain 'No env value for ENV_TEST is set.' when env 'ENV_TEST' is not set.:
+        web_1_1b99cff96784 |      AssertionError: expected '\n  - ENV_TEST: No env value for ENV_TEST is set. - Should return the cluster name.\n  - API Call: kth-azure-app - Should always return this applications name.\n  ' to include 'No env FAILJRED value for ENV_TEST is set'
+        web_1_1b99cff96784 |       at Context.<anonymous> (/tests/unit-tests/test-app.js:22:23)
+        web_1_1b99cff96784 |       at processTicksAndRejections (internal/process/task_queues.js:93:5)
+        web_1_1b99cff96784 | 
+        web_1_1b99cff96784 | 
+        web_1_1b99cff96784 | 
+        web_1_1b99cff96784 | npm ERR! Test failed.  See above for more details.
+        web_1_1b99cff96784 | kth-azure-app_web_1_1b99cff96784 exited with code 1
+        Aborting on container exit...
+        '''
 
+        step = UnitTestStep()
+
+        clean_text = step.get_stack_trace_shortend(ci_output)
+
+        print(clean_text)
+        self.assertFalse('npm ERR!' in clean_text)
